3d5b994c7b35a3e83de7e458efdb6e2defe35f5d
Okocha <oko23015@byui.edu>
2025-01-18 21:19:53 -0800

    my commit

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,6 +1,9 @@
 const date = new Date(document.lastModified);
 document.getElementById("lastModified").innerHTML = `Last modified: ${date}`;
 
+const currentYear = new Date().getFullYear();
+document.getElementById("year").textContent = currentYear;
+
 const pageVisitCountDisplay = document.getElementById("pageVisitCount");
     let pageVisitCount = localStorage.getItem("pageVisitCount");
     if (pageVisitCount) {  
