f55e7f017302babaea50d0b0b4807f94853de27f
Okocha <oko23015@byui.edu>
2025-01-28 14:34:00 -0800

    commit

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -25,7 +25,31 @@ const navigation = document.querySelector('.my-link');
 
 hamButton.addEventListener('click', () => {
     navigation.classList.toggle('show'); // Toggle the display class
-    hamButton.textContent = navigation.classList.contains('show') ? '❌' : '☰';
+    hamButton.textContent = navigation.classList.contains('show') ? '' : '☰';
 });
 
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+const header = document.querySelector("header");
+const footer = document.querySelector("footer");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("🕶️")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+        header.style.background = "#000";
+		header.style.color = "#fff";
+        footer.style.background = "#000";
+        footer.style.color = "#fff";
+		modeButton.textContent = "🔆";
+	} else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+        header.style.background = "navy";
+		header.style.color = "white";
+        footer.style.background = "navy";
+        footer.style.color = "white";
+		modeButton.textContent = "🕶️";
+	}
+});
 
