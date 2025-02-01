9c066e10c408188cbe015c779e30815ed1bc648d
Okocha <oko23015@byui.edu>
2025-01-31 23:25:26 -0800

    commit

---
+++ chamber/scripts/chamber.js
@@ -0,0 +1,29 @@
+const date = new Date(document.lastModified);
+document.getElementById("lastModified").innerHTML = `Last modified: ${date}`;
+
+
+
+const currentYear = new Date().getFullYear();
+document.getElementById("year").textContent = currentYear;
+
+
+const pageVisitCountDisplay = document.getElementById("pageVisitCount");
+    let pageVisitCount = localStorage.getItem("pageVisitCount");
+    if (pageVisitCount) {  
+        pageVisitCount = parseInt(pageVisitCount) + 1;
+    } else {  
+        pageVisitCount = 1;
+    }
+    localStorage.setItem("pageVisitCount", pageVisitCount);
+
+    if (pageVisitCountDisplay) {
+        pageVisitCountDisplay.textContent = `Page Visit: ${pageVisitCount} time(s).`;
+    }
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.my-link');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('show'); 
+    hamButton.textContent = navigation.classList.contains('show') ? '' : '☰';
+});
